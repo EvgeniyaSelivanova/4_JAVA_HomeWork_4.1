f716d1aa24a72d9ba29c759ac5430cc9391ab02c
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2020-12-25 16:28:19 +0300

    4.1_Final Version.1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(float ticket) {
+
+        int equivalent = 20;
+
+        if (ticket > 0) {
+            return (int) ticket / equivalent;
+        }
+        return 0;
+    }
+}
+
